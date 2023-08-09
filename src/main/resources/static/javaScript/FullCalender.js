70a26ca3c0d3be17b21106cf300c9529776703bf
TunaWeiWei <somacruz23759139@gmail.com>
2023-08-09 12:22:49 +0800

    8/9

--- src/main/resources/static/javaScript/FullCalender.js
+++ src/main/resources/static/javaScript/FullCalender.js
@@ -7,8 +7,26 @@ document.addEventListener('DOMContentLoaded', function() {
           headerToolbar: {       	  
         	  left: 'prev,next today',
         	  center: 'title',
-        	  right: 'dayGridMonth,timeGridDay'
+        	  right: 'dayGridMonth,timeGridDay'     	  
+        	  
           },
+          	  events: [{
+                 title: '公休',
+                 start: '2023-08-09',
+                 end: '2023-08-14'
+             },
+             {
+                 title: '公休',
+                 start: '2023-08-21',
+                 end: '2023-08-25'
+             },
+             {
+                 title: '公休',
+                 start: '2023-08-27',
+                 end: '2023-08-27'
+             }
+
+         ]
           
         });
         calendar.render();
