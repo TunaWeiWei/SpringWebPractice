aca1d4c79acc5a091d5d20d8d7f2a724d29ec354
TunaWeiWei <somacruz23759139@gmail.com>
2023-08-10 13:41:27 +0800

    8/10 v2

--- src/main/resources/static/javaScript/FullCalender.js
+++ src/main/resources/static/javaScript/FullCalender.js
@@ -13,12 +13,12 @@ document.addEventListener('DOMContentLoaded', function() {
           	  events: [{
                  title: '元旦',
                  start: '2023-01-01',
-                 end: '2023-01-01'
+                 end: '2023-01-02'
              },
              {
                  title: '春節',
                  start: '2023-01-20',
-                 end: '2023-01-27'
+                 end: '2023-01-28'
              },
              {
                  title: '228 紀念日連假',
@@ -28,32 +28,32 @@ document.addEventListener('DOMContentLoaded', function() {
 			 {
                  title: '兒童節連假',
                  start: '2023-04-03',
-                 end: '2023-04-04'
+                 end: '2023-04-05'
              },
              {
                  title: '清明節',
                  start: '2023-04-05',
-                 end: '2023-04-05'
+                 end: '2023-04-06'
              },
              {
                  title: '勞動節',
                  start: '2023-05-01',
-                 end: '2023-05-01'
+                 end: '2023-05-02'
              },
              {
                  title: '端午節連假',
                  start: '2023-06-22',
-                 end: '2023-06-23'
+                 end: '2023-06-24'
              },
              {
                  title: '中秋節',
                  start: '2023-09-29',
-                 end: '2023-09-29'
+                 end: '2023-09-30'
              },
              {
-                 title: '國慶日',
+                 title: '國慶日連假',
                  start: '2023-10-09',
-                 end: '2023-1002-10'
+                 end: '2023-10-11'
              },
          ]
           
